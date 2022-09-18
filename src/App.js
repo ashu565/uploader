ca879d367162e61944d9fb4084ba176a000de17c
Ashutosh Singh <ashu9993063472@gmail.com>
2022-09-18 17:24:21 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Upload } from "@aws-sdk/lib-storage";
+import { S3Client, S3 } from "@aws-sdk/client-s3";
+const App = () => {
+  function fileChange(e) {
+    var file = e.target.files[0];
+    // console.log(e.target.files[0])
 
-function App() {
+    const creds = {
+      accessKeyId: process.env.REACT_APP_ACCESSKEYID,
+      secretAccessKey: process.env.REACT_APP_SECRETACCESSKEY,
+    };
+    try {
+      const parallelUploads3 = new Upload({
+        client: new S3Client({ region: "us-west-2", credentials: creds }),
+        leavePartsOnError: false, // optional manually handle dropped parts
+        params: {
+          Bucket: process.env.REACT_APP_AWSBUCKET,
+          Key: file.name,
+          Body: file,
+        },
+      });
+
+      parallelUploads3.on("httpUploadProgress", (progress) => {
+        console.log(progress);
+        let a = getUploadingProgress(progress.loaded, progress.total);
+        console.log(a);
+      });
+
+      parallelUploads3.done();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  function getUploadingProgress(uploadSize, totalSize) {
+    let uploadProgress = (uploadSize / totalSize) * 100;
+    return Number(uploadProgress.toFixed(0));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="UploadContainer">
+      <h1>Upload your content here ..</h1>
+      <input type="file" onChange={fileChange} />
     </div>
   );
-}
+};
 
 export default App;
